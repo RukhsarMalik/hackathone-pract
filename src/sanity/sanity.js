37e8918eb47f31a470f00a2a13c691b42bd00f35
RukhsarMalik <103738665+RukhsarMalik@users.noreply.github.com>
2025-01-14 20:43:42 +0500

    last

--- src/sanity/sanity.js
+++ src/sanity/sanity.js
@@ -5,7 +5,7 @@ const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,     // Environment variable for dataset
   apiVersion: '2023-01-01',                            // Use the latest or specific API version
   useCdn: process.env.NODE_ENV === 'production',       // Use CDN in production for faster response
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,         // Optional: Include token if fetching private data
+  token: process.env.SANITY_TOKEN,         // Optional: Include token if fetching private data
 });
 
 export default client;
